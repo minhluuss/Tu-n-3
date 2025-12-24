26db634db94debf0b324ff9b67892ef1df18f7b3
TheMinh <luutheminh15082004@gmail.com>
2025-12-24 12:51:29 +0700

    update

--- Gioi_Thieu.php
+++ Gioi_Thieu.php
@@ -3,18 +3,87 @@
     include 'header.php'; 
 ?>
 
+<div id="khoangtrang" class="container-fluid">
+
+</div>
+<section class="banner-gioi-thieu">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <div class="col-lg-6 col-md-12 position-relative lop-tren-cung">
+                <div class="noi-dung-chu">
+                    Giới thiệu chương trình<br>
+                    Sức khỏe học đường
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="phan-ben-phai">
+        <div class="day-trang-tri">
+            <img src="images/dayxanhvang.png" alt="dây xanh vàng">
+        </div>
+
+        <div class="anh-nen">
+            <img src="images/anhhocsinh.png" alt="Học sinh">
+        </div>
+    </div>
+</section>
 
 
 
+<section id="phan-noi-dung" class="py-5">
+    <div class="container">
+        
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8 text-left">
+                <p id="quyet-dinh-1660" class="font-weight-bold" style="font-size: 2rem;">
+                    Ngày 02/10/2021, Chính phủ đã ban hành quyết định 1660 phê duyệt Chương trình Sức khỏe học đường giai đoạn 2021-2025
+                </p>
+                <p>
+                    Nhằm bảo đảm chăm sóc toàn diện về thể chất, tinh thần cho trẻ em, học sinh thông qua việc duy trì, đẩy mạnh hoạt động giáo dục, chăm sóc, bảo vệ và quản lý sức khỏe trẻ em, học sinh trong các cơ sở giáo dục mầm non, phổ thông và các trường chuyên biệt.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-5 col-md-12">
+                <div class="khoi-noi-dung mau-xanh">
+                    
+                    <div class="so-lon shadow-sm">
+                        <h2>05</h2>
+                        <span>nhóm nội dung</span>
+                    </div>
 
+                    <div class="danh-sach-muc">
+                        <div class="muc-con">Chăm sóc, bảo vệ và quản lý sức khỏe học sinh trong trường học</div>
+                        <div class="muc-con">Công tác giáo dục thể chất và hoạt động thể thao trong trường học</div>
+                        <div class="muc-con">Tổ chức bữa ăn học đường, bảo đảm dinh dưỡng hợp lý</div>
+                        <div class="muc-con">Tuyên truyền, giáo dục sức khỏe học đường trong trường học</div>
+                        <div class="muc-con">Ứng dụng công nghệ thông tin vào chăm sóc, bảo vệ, quản lý sức khỏe</div>
+                    </div>
+                </div>
+            </div>
 
+            <div class="col-lg-2 d-none d-lg-block"></div>
 
+            <div class="col-lg-5 col-md-12">
+                <div class="khoi-noi-dung mau-cam">
+                    
+                    <div class="so-lon shadow-sm">
+                        <h2>07</h2>
+                        <span>nhóm giải pháp</span>
+                    </div>
 
+                    <div class="danh-sach-muc">
+                        <div class="muc-con">Bổ sung, nâng cấp cơ sở vật chất, trang thiết bị trong trường học</div>
+                        <div class="muc-con">Tăng cường và nâng cao chất lượng nguồn nhân lực</div>
+                        <div class="muc-con">Đổi mới công tác giáo dục thể chất, hoạt động thể thao</div>
+                        </div>
+                </div>
+            </div>
+        </div>
 
-
-
-
-
+    </div>
+</section>
 
 
 <?php
