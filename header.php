4145e59002bbcf4069c050555d86ee63532399d2
TheMinh <luutheminh15082004@gmail.com>
2025-12-24 18:27:51 +0700

    update

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="header.css">
-    <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="style.css">
 
 
     <?php if (isset($ten_css)) { echo $ten_css; } ?>
