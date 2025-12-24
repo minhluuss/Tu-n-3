5e9c35787702ba5e122d16f8e5696839067e9a19
TheMinh <luutheminh15082004@gmail.com>
2025-12-24 18:23:36 +0700

    update

--- footer.php
+++ footer.php
@@ -1,5 +1,115 @@
- <footer >
-        
+ <section class="khu-vuc-lien-he">
+        <div class="khung-noi-dung khung-chung">
+
+            <!-- BÊN TRÁI -->
+            <div class="noi-dung-trai">
+                <img src="images/thathskm.png" alt="">
+            </div>
+
+            <!-- BÊN PHẢI -->
+            <div class="hop-lien-he">
+                <h3 class="tieu-de-form">Liên hệ</h3>
+
+                <form id="form-lien-he">
+                    <input type="email" class="o-nhap" placeholder="Email">
+                    <input type="text" class="o-nhap" placeholder="Số điện thoại">
+                    <textarea class="o-nhap o-noi-dung" placeholder="Nội dung"></textarea>
+
+                    <div class="nhom-lua-chon">
+                        <label class="lua-chon">
+                            <input type="radio" name="loai-lien-he">
+                            Liên hệ truyền thông
+                        </label>
+                        <label class="lua-chon">
+                            <input type="radio" name="loai-lien-he">
+                            Liên hệ đồng hành
+                        </label>
+                    </div>
+
+                    <button type="submit" id="nut-gui-lien-he">
+                        LIÊN HỆ NGAY
+                    </button>
+                </form>
+            </div>
+        </div>
+    </section>
+ <footer class="chan-trang">
+
+        <div class="khung-footer khung-chung">
+
+            <!-- NHÓM CỘT TRÁU -->
+            <div class="cot-footer cot-thong-tin">
+                <div class="logo-footer">
+                    <img src="images/Logoft.png" alt="Sức khỏe học đường">
+                </div>
+
+                <p class="giay-phep">
+                    Giấy phép số 100/**-***** do Bộ Thông tin và Truyền thông cấp
+                </p>
+
+                <p>Liên hệ chương trình: Công ty TNHH Truyền thông UNICOM </p>
+                <p>Địa chỉ: Tầng 4. Toà nhà An Bình, số 3, Trần Nguyên Đán, Định Công, Hoàng Mai, Hà Nội</p>
+                <p>Điện thoại: 024 3640 0671</p>
+                <p>Email:</p>
+
+                <p><br>Kết nối với chúng tôi tại</p>
+                <div class="mang-xa-hoi">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+
+            <!-- NHÓM CỘT PHẢI-->
+            <div class="nhom-cot-phai">
+                <!-- CỘT 1: TIN TỨC / HỌC LIỆU -->
+                <div class="cot-footer">
+                    <h4>Tin tức</h4>
+                    <ul>
+                        <li><a href="#">Tin tức</a></li>
+                        <li><a href="#">Sức khỏe học đường TV</a></li>
+                        <li><a href="#">Hỏi đáp cùng chuyên gia</a></li>
+                        <li><a href="#">Thông cáo báo chí</a></li>
+                    </ul>
+
+                    <h4>Học liệu</h4>
+                    <ul>
+                        <li><a href="#">Mầm non</a></li>
+                        <li><a href="#">Tiểu học</a></li>
+                        <li><a href="#">THCS</a></li>
+                        <li><a href="#">THPT</a></li>
+
+                    </ul>
+                </div>
+
+                <!-- CỘT 2: DỰ ÁN -->
+                <div class="cot-footer">
+                    <h4>Dự án</h4>
+                    <ul>
+                        <li><a href="#">Nước sạch học đường</a></li>
+                        <li><a href="#">Dinh dưỡng học đường</a></li>
+                        <li><a href="#">Nha học đường</a></li>
+                        <li><a href="#">Bệnh học đường</a></li>
+                        <li><a href="#">An toàn thực phẩm</a></li>
+                        <li><a href="#">Chống bạo lực học đường</a></li>
+                        <li><a href="#">An toàn trên môi trường mạng</a></li>
+                        <li><a href="#">Giáo dục giới tính</a></li>
+                        <li><a href="#">Tâm lý học đường</a></li>
+                    </ul>
+                </div>
+
+                <!-- CỘT 3: MENU PHỤ -->
+                <div class="cot-footer">
+                    <div class="cot-phu">
+                        <h4><strong class="noi-bat">E</strong>-magazine</h4>
+                        <h4>Giới thiệu</h4>
+                        <h4>Liên hệ</h4>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <title>Web</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="footer.css">
 
@@ -14,6 +14,63 @@
     <?php if (isset($ten_css)) { echo $ten_css; } ?>
 </head>
 <body>
-    <header >
+    <header class="dau-trang">
 
+        <!-- THANH TREN -->
+        <div class="thanh-tren">
+            <div class="khung-ben-tren khung-chung">
+
+                <div class="thong-tin-trai">
+                    <img src="images/Logo.png" class="logo">
+                    <span class="ngay-thang">Thứ ba 02-01-2024</span>
+                    <span class="dia-diem">Hà Nội </span>
+                    <span class="ds-dia-diem">⌄</span>
+                    <span class="thoi-tiet">
+                        <img src="images/drizzle.png" alt="thoi tiet">
+                        26.2°C
+                    </span>
+                </div>
+
+                <div class="thong-tin-phai">
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                    <span class="co-quoc-gia">
+                        <svg viewBox="0 0 512 512">
+                            <rect width="512" height="512" fill="#DA251D" />
+                            <polygon fill="#FFDD00" points="256,96 293,208 411,208 315,272
+                                                            351,384 256,320 161,384 197,272
+                                                            101,208 219,208" />
+                        </svg>
+                    </span>
+                </div>
+
+            </div>
+        </div>
+        <!-- THANH MENU -->
+        <nav class="thanh-menu">
+            <div class="khung-menu khung-chung">
+
+                <ul class="danh-sach-menu">
+                    <li><a href="#" class="home">
+                            <img src="images/Home.png" alt="home">
+                            Giới thiệu
+                        </a></li>
+                    <li><a href="#">Tin tức</a></li>
+                    <li><a href="#">Dự án</a></li>
+                    <li><a href="#">Học liệu</a></li>
+                    <li><a href="#"><strong class="noi-bat">E</strong>-magazine</a></li>
+                    <li><a href="#">Liên hệ</a></li>
+                </ul>
+
+                <div class="tim-kiem">
+                    <input type="text" placeholder="Tìm kiếm thông tin">
+                    <button class="btn-tim-kiem">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                </div>
+
+            </div>
+        </nav>
     </header>
