49438ed1dbab0aca67a32d83f6847ddc429254df
TheMinh <luutheminh15082004@gmail.com>
2025-12-25 15:16:43 +0700

    update

--- Gioi_Thieu.php
+++ Gioi_Thieu.php
@@ -33,57 +33,102 @@
 
 <section id="phan-noi-dung" class="py-5">
     <div class="container">
-        
-        <div class="row justify-content-center mb-5">
-            <div class="col-md-8 text-left">
-                <p id="quyet-dinh-1660" class="font-weight-bold" style="font-size: 2rem;">
+    <div class="noi-dung" >
+        <div class="justify-content-center mb-5">
+            <div class="text-start">
+                <p id="quyet-dinh-1660" class="font-weight-bold mb-3" style="font-size: 2rem;">
                     Ngày 02/10/2021, Chính phủ đã ban hành quyết định 1660 phê duyệt Chương trình Sức khỏe học đường giai đoạn 2021-2025
                 </p>
-                <p>
+                <p id="quyetdinh">
                     Nhằm bảo đảm chăm sóc toàn diện về thể chất, tinh thần cho trẻ em, học sinh thông qua việc duy trì, đẩy mạnh hoạt động giáo dục, chăm sóc, bảo vệ và quản lý sức khỏe trẻ em, học sinh trong các cơ sở giáo dục mầm non, phổ thông và các trường chuyên biệt.
                 </p>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-5 col-md-12">
-                <div class="khoi-noi-dung mau-xanh">
-                    
-                    <div class="so-lon shadow-sm">
-                        <h2>05</h2>
-                        <span>nhóm nội dung</span>
-                    </div>
-
-                    <div class="danh-sach-muc">
-                        <div class="muc-con">Chăm sóc, bảo vệ và quản lý sức khỏe học sinh trong trường học</div>
-                        <div class="muc-con">Công tác giáo dục thể chất và hoạt động thể thao trong trường học</div>
-                        <div class="muc-con">Tổ chức bữa ăn học đường, bảo đảm dinh dưỡng hợp lý</div>
-                        <div class="muc-con">Tuyên truyền, giáo dục sức khỏe học đường trong trường học</div>
-                        <div class="muc-con">Ứng dụng công nghệ thông tin vào chăm sóc, bảo vệ, quản lý sức khỏe</div>
-                    </div>
-                </div>
             </div>
+        </div>
 
-            <div class="col-lg-2 d-none d-lg-block"></div>
+        <!-- ROW -->
+        <div class="row align-items-stretch">
 
-            <div class="col-lg-5 col-md-12">
-                <div class="khoi-noi-dung mau-cam">
-                    
-                    <div class="so-lon shadow-sm">
-                        <h2>07</h2>
-                        <span>nhóm giải pháp</span>
+            <!-- CỘT TRÁI -->
+            <div class="col-lg-5 col-md-12 d-flex">
+                <div class="cot-wrapper w-100">
+
+                    <div class="mo-ta">
+                        Chương trình Sức khoẻ học đường giai đoạn 2021-2025 xác định
                     </div>
 
-                    <div class="danh-sach-muc">
-                        <div class="muc-con">Bổ sung, nâng cấp cơ sở vật chất, trang thiết bị trong trường học</div>
-                        <div class="muc-con">Tăng cường và nâng cao chất lượng nguồn nhân lực</div>
-                        <div class="muc-con">Đổi mới công tác giáo dục thể chất, hoạt động thể thao</div>
+                    <div class="khoi-noi-dung mau-xanh flex-fill">
+                        <div class="so-lon shadow-sm">
+                            <h2>05</h2>
+                            <span>nhóm nội dung</span>
                         </div>
+
+                        <div class="danh-sach-muc">
+                            <div class="muc-con">Chăm sóc, bảo vệ và quản lý sức khỏe học sinh trong trường học</div>
+                            <div class="muc-con">Công tác giáo dục thể chất và hoạt động thể thao trong trường học</div>
+                            <div class="muc-con">Tổ chức bữa ăn học đường, bảo đảm dinh dưỡng hợp lý trong trường học</div>
+                            <div class="muc-con">Tuyên truyền, giáo dục sức khỏe học đường trong trường học</div>
+                            <div class="muc-con">Ứng dụng công nghệ thông tin vào chăm sóc, bảo vệ, quản lý sức khỏe học sinh và thống kê, báo cáo trong trường học</div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
-        </div>
 
+            <!-- CỘT PHẢI -->
+            <div class="col-lg-5 col-md-12 d-flex">
+                <div class="cot-wrapper w-100">
+
+                    <div class="khoi-noi-dung mau-cam flex-fill">
+                        <div class="so-lon shadow-sm">
+                            <h2>07</h2>
+                            <span>nhóm giải pháp</span>
+                        </div>
+
+                        <div class="danh-sach-muc">
+                            <div class="muc-con">Bổ sung, nâng cấp cơ sở vật chất, trang thiết bị trong trường học</div>
+                            <div class="muc-con">Tăng cường và nâng cao chất lượng nguồn nhân lực</div>
+                            <div class="muc-con">Đổi mới công tác giáo dục thể chất, hoạt động thể thao trường học</div>
+                            <div class="muc-con">Tuyên truyền, giáo dục sức khỏe học đường trong trường học</div>
+                            <div class="muc-con">Tuyên truyền, giáo dục sức khỏe học đường trong trường học</div>
+                            <div class="muc-con">Tăng cường công tác truyền thông, giáo dục và vận động xã hội</div>
+                            <div class="muc-con">Hoàn thiện cơ chế, chính sách, phối hợp liên ngành, tăng cường trách nhiệm triển khai</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     </div>
+    <div class="justify-content-center">
+            <div class="text-start mb-5 mt-5 ">
+                <div class="noi-dung">
+                <p id="noi-dung-chuong-trinh">
+                    Với chương trình này,<strong> hơn 22 triệu trẻ em, học sinh (tương đương 25% dân số Việt Nam) được chăm sóc sức khỏe ban đầu ngay từ trong trường học.</strong> Điều này sẽ tiết kiệm được khoản kinh phí lớn dành cho công tác điều trị, giảm bớt gánh nặng của bệnh tật cho gia đình và toàn xã hội, cải thiện tầm vóc, chất lượng giống nòi.
+                </p>
+                <br>
+                <p>
+                    Chương trình cũng tạo cơ chế chính sách, đẩy mạnh công tác xã hội hóa nhằm thu hút sự tham gia đầu tư của các tổ chức, doanh nghiệp trong và ngoài nước; huy động các nguồn lực tăng cường cơ sở vật chất, trang thiết bị phục vụ cho việc chăm sóc sức khỏe trẻ em, học sinh.
+                </p>
+            </div>
+            </div>
+        </div>
+        <div class="thong-diep text-center font-weight-bold mb-3" style="font-size: 1.5rem;">
+            Với thông điệp
+        </div>
+        <div class="truong-hoc font-weight-bold text-center">
+        <h2 >Trường học an toàn</h2>
+        <h2>học sinh khỏe mạnh</h2>
+    </div>
+    <div class="justify-content-center">
+    <p class="noidungthongdiep text-start mt-4">
+        Lần đầu tiên tại Việt Nam một chương trình quy mô, thiết thực và đầy ý nghĩa về sức khoẻ học đường với chương trình tổng thể, chính thống và toàn diện được thực hiện, cùng với đó là trách nhiệm và hy vọng lớn lao về những ngôi trường an toàn, thân thiện cho trẻ em, học sinh.
+    </p>
+</div>
 </section>
+
+
 
 
 <?php
